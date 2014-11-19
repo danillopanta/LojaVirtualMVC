c27f161970652683858f5c60bf11beeaf82a001b
danillopanta <danillopanta@hotmail.com>
2014-11-19 18:47:06 -0200

    Classes de Paginacao

--- LojaVirtual.Web/Controllers/ProdutoController.cs
+++ LojaVirtual.Web/Controllers/ProdutoController.cs
@@ -9,14 +9,14 @@ namespace LojaVirtual.Web.Controllers
 {
     public class ProdutoController : Controller
     {
-        private ProdutosRepositorio _repositorio;
+        
         // GET: Produto
-        public ActionResult Index()
-        {    
-            _repositorio = new ProdutosRepositorio();
-            var produtos = _repositorio.Produtos.Take(10);
+        //public ActionResult Index()
+        //{    
+        //    //_repositorio = new ProdutosRepositorio();
+        //    //var produtos = _repositorio.Produtos.Take(10);
 
-            return View(produtos);
-        }
+        //    //return View(produtos);
+        //}
     }
 }
